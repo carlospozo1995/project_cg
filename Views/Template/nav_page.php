bdeb9b39a698922763a1c95a166555a80f7a8dca
Carlos Pozo <carlospozo95@gmail.com>
2022-07-08 12:18:02 -0500

    comenzando css

--- Views/Template/nav_page.php
+++ Views/Template/nav_page.php
@@ -1,9 +1,9 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4 aside-mc">
     <!-- Brand Logo -->
     <a href="<?php base_url(); ?>sistema" class="brand-link">
       <img src="<?= media(); ?>images/logo_cg.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Créditos GUAMAN</span>
+      <span class="title-s-mc">Créditos GUAMAN</span>
     </a>
 
     <!-- Sidebar -->
