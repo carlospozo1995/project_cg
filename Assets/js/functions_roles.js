3fa788722c7d1f08345e6800ce4ad4dea5bee7ee
carlospozo1995 <carlospozo95@gmail.com>
2022-07-08 12:00:51 -0500

    final botones datatable

--- Assets/js/functions_roles.js
+++ Assets/js/functions_roles.js
@@ -28,24 +28,12 @@ document.addEventListener('DOMContentLoaded', function () {
         initComplete: function(){
             
         },
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    });
 })
-
-
-
-
 
 // OPEN MODAL ROL
 $("#btnNewRol").click(function openModal() {
     $('#modalFormRol').modal('show');
 })
-
-
-
-// $("#example1").DataTable({
-//     "responsive": true, "lengthChange": true, "autoWidth": false,
-//     "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"],
-//     "lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "Todos"] ],
-// }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
 
 
