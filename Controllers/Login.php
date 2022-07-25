6e45e025faa79d49c9823c535370f1763cda9f84
Carlos Pozo <carlospozo95@gmail.com>
2022-07-24 22:47:29 -0500

    work login

---
+++ Controllers/Login.php
@@ -0,0 +1,22 @@
+<?php
+
+    class Login extends Controller{
+
+        public function __construct()
+        {
+            parent::__construct();
+        }
+        
+        public function login()
+        {
+            $data['page_tag'] = 'Login';
+            $data['page_title'] = 'Login';
+            $data['page_name'] = 'Login';
+            $data['page_functions_js'] = 'functions_login.js';
+            $this->views->getView($this, "login", $data);
+        }
+    }
+
+
+
+?>
