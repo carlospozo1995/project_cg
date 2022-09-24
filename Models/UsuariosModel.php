3e16d3da8b02b0a9263807a6d73ac9c5656ad97a
carlospozo1995 <carlospozo95@gmail.com>
2022-09-23 23:47:36 -0500

    update  status 0 usuarios

--- Models/UsuariosModel.php
+++ Models/UsuariosModel.php
@@ -136,19 +136,10 @@
         public function deleteUser(int $idusuario)
         {
             $this->intIdUsuario = $idusuario;
-            $sql_delete_user = "DELETE FROM project_cg.usuario WHERE idusuario = $this->intIdUsuario";
-            $request = $this->delete($sql_delete_user);
+            $sql_update_status_user = "UPDATE project_cg.usuario SET status = 0 WHERE idusuario = $this->intIdUsuario";
+            $request = $this->update($sql_update_status_user);
 
             if ($request) {
-                $sql_reset_one = "SET @autoiduser=0";
-                $this->resetDT($sql_reset_one);
-                
-                $sql_reset_dos = "UPDATE project_cg.usuario SET idusuario = (@autoiduser := @autoiduser + 1)";
-                $this->resetDT($sql_reset_dos);
-
-                $sql_reset_three = "ALTER TABLE project_cg.usuario AUTO_INCREMENT = 1";
-                $this->resetDT($sql_reset_three);
-                
                 $request = "ok";
             }else{
                 $request = "error";
