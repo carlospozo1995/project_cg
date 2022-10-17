a9ef943053aecb0b22581b9c82b9f6bdf6b5c89b
carlospozo1995 <carlospozo95@gmail.com>
2022-10-17 00:50:50 -0500

    work structure and style page

--- Views/index.php
+++ Views/index.php
@@ -8,14 +8,97 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;1,100;1,200;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet"> 
+    <link rel="stylesheet" href="<?php echo media(); ?>ecommerce/css/plugins/fontawesome/css/all.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo media(); ?>ecommerce/css/plugins/animate/animate.css">
     <link rel="stylesheet" type="text/css" href="<?php echo media(); ?>ecommerce/css/main.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo media(); ?>ecommerce/css/mainTwo.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo media(); ?>ecommerce/css/normalize.css">
     <title><?= $data['page_title'] ?></title>
 </head>
-<body>
-
+<body class="animsition">
+    <p>prueba</p>
     <header>
-        <p>hola</p>
+        <div class="container-menu-desktop">
+
+            <div class="top-bar">
+                <div class="content-topbar flex-sb-m h-full container">
+                    <div class="left-top-bar">
+                        Envios gratis desde $100
+                    </div>
+
+                    <div class="right-top-bar flex-w h-full">
+                        <a href="#" class="flex-c-m trans-04 p-lr-25">
+                            Ayuda / Preguntas frecuentes
+                        </a>
+
+                        <a href="#" class="flex-c-m trans-04 p-lr-25">
+                            Iniciar Sesión
+                        </a>
+
+                        <span class="flex-c-m trans-04 p-lr-25">
+                            Atención al cliente: 0123456789
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="wrap-menu-desktop">
+                <nav class="limiter-menu-desktop container">
+                        
+                    <a href="#" class="logo">
+                        <img src="<?php echo media(); ?>ecommerce/images/logo_text.png" alt="IMG-LOGO">
+                    </a>
+
+                    <div class="menu-desktop">
+                        <ul class="main-menu">
+                            <li class="active-menu">
+                                <a href="index.html">Home</a>
+                                <ul class="sub-menu">
+                                    <li><a href="index.html">Homepage 1</a></li>
+                                    <li><a href="home-02.html">Homepage 2</a></li>
+                                    <li><a href="home-03.html">Homepage 3</a></li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="product.html">Shop</a>
+                            </li>
+
+                            <li class="label1" data-label1="hot">
+                                <a href="shoping-cart.html">Features</a>
+                            </li>
+
+                            <li>
+                                <a href="blog.html">Blog</a>
+                            </li>
+
+                            <li>
+                                <a href="about.html">About</a>
+                            </li>
+
+                            <li>
+                                <a href="contact.html">Contact</a>
+                            </li>
+                        </ul>
+                    </div>  
+
+                    <!-- Icon header -->
+                    <div class="wrap-icon-header flex-w flex-r-m">
+                        <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </div>
+
+                        <div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="2">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                        </div>
+
+                        <a href="#" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
+                            <i class="fa-solid fa-heart"></i>
+                        </a>
+                    </div>
+                </nav>
+            </div>  
+        </div>
     </header>
 
 </body>
