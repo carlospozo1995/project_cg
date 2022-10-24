fc21f82a0c629062390970890836cce0e5e7109a
carlospozo1995 <carlospozo95@gmail.com>
2022-10-24 10:52:46 -0500

    work slider

--- Views/index.php
+++ Views/index.php
@@ -154,10 +154,62 @@
         </div>
     </header>
 
-    <section class="limiter-fixed-scroll section-slider">
-        <img src="<?= media(); ?>ecommerce/images/slider-two.png" style="width: 100%;">
+    <section class="section-slide">
+        <div class="wrap-slick1">
+            <div class="slick1">
+                <div class="item-slick1" style="background-image: url(<?= media(); ?>ecommerce/images/slider.png)">
+                    <div class="container h-full">
+                        <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
+                            <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
+                                <span class="ltext-101 cl2 respon2">
+                                    Women Collection 2018
+                                </span>
+                            </div>
+                                
+                            <div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
+                                <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
+                                    NEW SEASON
+                                </h2>
+                            </div>
+                                
+                            <div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
+                                <a href="product.html" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
+                                    Shop Now
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="item-slick1" style="background-image: url(<?= media(); ?>ecommerce/images/slider-two.png)">
+                    <div class="container h-full">
+                        <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
+                            <div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
+                                <span class="ltext-101 cl2 respon2">
+                                    Men New-Season
+                                </span>
+                            </div>
+                                
+                            <div class="layer-slick1 animated visible-false" data-appear="lightSpeedIn" data-delay="800">
+                                <h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
+                                    Jackets & Coats
+                                </h2>
+                            </div>
+                                
+                            <div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
+                                <a href="product.html" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
+                                    Shop Now
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </section>
-    <section class="container-pages">
+
+    <!-- <section class="container-pages">
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -170,7 +222,7 @@
         consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
         proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </section>
+    </section> -->
 
     <footer></footer>
 
@@ -182,7 +234,7 @@
     <script src="<?= media(); ?>ecommerce/plugins/bootstrap/js/bootstrap.min.js"></script>
     <!--===============================================================================================-->
     <script src="<?= media(); ?>ecommerce/plugins/slick/slick.min.js"></script>
-    <!-- <script src="<?= media(); ?>ecommerce/js/slick-custom.js"></script> -->
+    <script src="<?= media(); ?>ecommerce/js/slick-custom.js"></script>
     <!--===============================================================================================-->
     <script src="<?= media(); ?>ecommerce/js/main.js"></script>
 
