233fd96e518d8654088bd7613be2f93353187e6c
carlospozo1995 <carlospozo95@gmail.com>
2022-11-15 14:44:01 -0500

    sin cambios

--- Config/config.php
+++ Config/config.php
@@ -8,7 +8,7 @@
     define('DB_NAME', 'project_cg');
     define('DB_USER', 'root');
     define('DB_PASSWORD', '');
-    define('PORT', 3306)
+    define('DB_PORT', 3306);
     define('DB_CHARSET', 'utf8');
 
     // ZONA HORARIA
